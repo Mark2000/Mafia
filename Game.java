dee88c868817dc2aac98034ca1bc9fcc7a2432e6
Mark <mark2000stephenson@gmail.com>
2017-05-04 15:29:45 -0700

    Added Game

---
+++ Game.java
@@ -0,0 +1,23 @@
+import java.util.*;
+public class Game
+{
+    Arraylist <Player> players = new Arraylist <>();
+    
+    public Game()
+    {
+        //add player type
+    }
+    
+    public void Main()
+    {
+        //SETUP
+        //get names
+        //assign roles
+        //show roles
+        //intro
+        //MAIN LOOP
+        //night
+        //day
+        //END
+    }
+}
