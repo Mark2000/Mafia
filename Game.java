ef0148d24dc59667d360c721f65d6a63ceefef3c
Mark <mark2000stephenson@gmail.com>
2017-05-05 14:17:23 -0700

    Names n stuff

--- Game.java
+++ Game.java
@@ -1,16 +1,34 @@
 import java.util.*;
 public class Game
 {
-    Arraylist <Player> players = new Arraylist <>();
+    private List<Player> players = new ArrayList<Player>();
     
-    public Game()
+    public Game(int n)//n = number of players; min 5
     {
-        //add player type
+        //1 detective, 1 medic, 2 mafia, 1 innocent
+    	players.add(new Detective());
+    	players.add(new Medic());
+    	for (int k = 0; k < Math.round((double)n / 3); k++)
+    		players.add(new Mafia());
+    	for (int k = 0; k < Math.round((double)2 * n / 3); k++)
+    		players.add(new Player());
     }
+    
+    private void setNames(ArrayList names)
+    {
+    	for (Player p : players)
+    	{
+    		int nameIndex = (int)(names.size() * Math.random());
+    		p.setName(names.get(nameIndex));
+    		names.remove(nameIndex);
+    	}
+    }
+    
+    
     
     public void Main()
     {
-        //SETUP
+        //SETUP2
         //get names
         //assign roles
         //show roles
@@ -19,5 +37,10 @@ public class Game
         //night
         //day
         //END
+    }
+    
+    public ArrayList<Player> updatePlayers()
+    {
+    	return players;
     }
 }
