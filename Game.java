236bd36fd5f4de3efe4e95de8a98716ce6bbd69b
Mark <mark2000stephenson@gmail.com>
2017-05-08 20:21:48 -0700

    Figured out using windows

--- Game.java
+++ Game.java
@@ -24,8 +24,6 @@ public class Game
     	}
     }
     
-
-    
     public List<Player> updatePlayers()
     {
     	return players;
--- UI.java
+++ UI.java
@@ -1,15 +1,48 @@
-public class UI
+import java.awt.*;
+import java.awt.event.*;
+import javax.swing.*;
+import java.util.concurrent.*;
+
+public class UI extends JFrame
 {
+	
+	public UI()
+	{
+		super("Mafia");
+		setLayout(new GridLayout(2,1));
+		setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+		setSize(800,600);
+		
+		
+		JPanel people = new JPanel(new GridLayout(0,4));
+		JPanel text = new JPanel();
+		
+		for (int i = 1; i < 14; i++)
+		{
+			people.add(new JButton("Player" + i));
+		}
+		
+		text.add(new JTextArea("This is sample text for the game of mafia. This is sample text for the game of mafia. This is sample text for the game of mafia. \n\n This is sample text for the game of mafia."));
+		
+		add(text);
+		add(people);
+		setVisible(true);
+		
+	}
+	
 	public static void main(String[] args)
     {
-        //SETUP
-        //get names
-        //assign roles
-        //show roles
-        //intro
-        //MAIN LOOP
-        //night
-        //day
-        //END
+		new UI();
     }
+	
+	//SETUP
+    //get names
+    //assign roles
+    //show roles
+    //intro
+    //MAIN LOOP
+    //night
+    //day
+    //END
+
 }
